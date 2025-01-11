b0e1d36083a74ba976e10c11a4cbfe6223fabb59
Francis Lagares <francis.lagares@gmail.com>
2025-01-11 16:56:02 +0100

    feat: project scaffolding

---
+++ src/tests/Home.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';
+
+import Home from '@/app/page';
+
+test('Pages Router', () => {
+  render(<Home />);
+
+  const button = screen.getByRole('button', { name: /Click me/i });
+
+  expect(button).toBeInTheDocument();
+});
