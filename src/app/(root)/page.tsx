be91ab501624b324eb8b811a1870939fe16dd41b
Francis Lagares <francis.lagares@gmail.com>
2025-01-12 12:37:05 +0100

    feat: create Home layout

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,12 @@
+import BookList from '@/components/BookList';
+import BookOverview from '@/components/BookOverview';
 import { Button } from '@/components/ui/button';
 
 const Home = () => {
   return (
     <>
+      <BookOverview />
+      <BookList />
       <Button>Click me</Button>
     </>
   );
